3db6cac918589f0f51df7b866650ab6fd8f1c1a6
supremeGlobal <supremeglobalbd@gmail.com>
2025-05-13 18:12:17 +0600

    Message part done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,71 +78,48 @@
 
     <section id="contact" class="section-7 form contact">
         <h2 class="text-center">Send a message</h2>
-        <div class="container">
-            <div class="row g-3">
-                <div class="col-12 col-md-8 pr-md-5 align-self-center text">
-                    <div class="row intro">
-                        <div class="col-12 p-0">
-                            <h3>Get in <span class="featured"><span>Touch</span></span></h3>
-                            <p>We will respond to your message as soon as possible.</p>
-                        </div>
+        <div class="container-fluid p-4 m-2">
+            <div class="row">
+                <div class="col-md-6">
+                    <div>
+                        <h3>Get in <span class="featured"><span>Touch</span></span></h3>
+                        <p>We will respond to your message as soon as possible.</p>
                     </div>
-                    <div class="row">
-                        <div class="col-12 p-0">
-                            <form action="php/form.php" id="nexgen-simple-form" class="nexgen-simple-form">
-                                <input type="hidden" name="section" value="nexgen_form">
-
-                                <input type="hidden" name="reCAPTCHA" data-key="6Lf-NwEVAAAAAPo_wwOYxFW18D9_EKvwxJxeyUx7"
-                                    value="03AFcWeA5XVjRikq-BrtUY3IxEUl8jSbqJT4Wnrlajk1yfTBvLUowIxjd5SsdYmyommzd2yJcHlejjU5t_wRtnYRAhmZZrgFfVuf9vUSE7RZWWyvXvj3UZJ9MTISZppw27_4K25T0TRBIouPLYULwTbwsa3fmW6n5ioqHV6MriwvNbSzEwrgIyIGonNG_EqfKrB9eflRI_e-BAqyEoD0lrb3H0sCfreoNFnwKsYOQEZIAs642uDz4pQh8Ez9rWOllfZ9P20nPPaIEVstBMZf2R6GWSKWH_ZRpxScWpQFOfAP3rhPkxn-TpENE2UuJmJ6DJ3oVV38KOaS9zJp2ZoAa6pM5PHMCspkC3MvAHiPbFQNmCYqit0oYG-l7LDQIHQZW27w8AIcJLVpFw-56cdrwqb4Byd-86p8GGi4UsBMQI4pxe2THpKxsufsjrxq_01eU_SY_vrlEsPuO6TBZ2lATwdlbKa7LMzmp6mSOsGdp7mORLgoK3FIKVVpkaYJc7zN_IcOYhcaHXuJyimJiocIqFCydytETycSOGV6IUyYF-zgAuECdPK_8WOPjnV15a2OTfTCElhvOQiXEtzZMW_BFrlTJMaFIfnwcLhHrWgfNzC0IFsgYx4fvdLfsGw27P5yTW6KtGbXEmDTXQEAbyIMZhb1LOasuBKpTpI9vpL87sac4a37pX4Wm9XbmwoPZ_6bZTAWzuQ2IlWPgvKGjOpSUazbZfmf_rjUzoNxA7guO7baKuyOQA7Rs4zr8oZoGRZaahSOCHSYYlMktY2sdoa21D21nOaXT3LAI7fwLsXqAJYEwIZXon7-0O_77dMEz0Jr3yWF8JNMAsGifCOFXH0evd02SshgvybFQvPMRCLj2yaihjsBycBhQKaT-tDHwPdZ7Q3CgZqtaeQI9-FXbolKKtwonQrSAflJLgcCbT5IQn0Yg-n5hwHPGNCANMwQlWvC8LBaW_fO2iBqRe">
-
-                                <div class="row form-group-margin">
-                                    <div class="col-6 col-md-6 m-0 p-2 input-group">
-                                        <input type="text" name="name" class="form-control field-name"
-                                            placeholder="Name">
-                                    </div>
-                                    <div class="col-6 col-md-6 m-0 p-2 input-group">
-                                        <input type="email" name="email" class="form-control field-email"
-                                            placeholder="Email">
-                                    </div>
-                                    <div class="col-6 col-md-6 m-0 p-2 input-group">
-                                        <input type="text" name="phone" class="form-control field-phone"
-                                            placeholder="Phone">
-                                    </div>
-                                    <div class="col-6 col-md-6 m-0 p-2 input-group">
-                                        <i class="icon-arrow-down mr-3"></i>
-                                        <select name="info" class="form-control field-info">
-                                            <option value="" selected="" disabled="">More Info</option>
-                                            <option>Audit &amp; Assurance</option>
-                                            <option>Financial Advisory</option>
-                                            <option>Analytics and M&amp;A</option>
-                                            <option>Middle Marketing</option>
-                                            <option>Legal Consulting</option>
-                                            <option>Regulatory Risk</option>
-                                            <option>Other</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-12 m-0 p-2 input-group">
-                                        <textarea name="message" class="form-control field-message" placeholder="Message"></textarea>
-                                    </div>
-                                    <div class="col-12 col-12 m-0 p-2 input-group">
-                                        <span class="form-alert" style="display: none;"></span>
-                                    </div>
-                                    <div class="col-12 input-group m-0 p-2">
-                                        <a class="btn primary-button">SEND</a>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        kkk
-                                    </div>
-                                    <div class="com-md-6">asasas</div>
-                                </div>
-                            </form>
+                    <form action="php/form.php" id="nexgen-simple-form" class="nexgen-simple-form">                               
+                        <div class="row">
+                            <div class="col-md-6 m-0 p-2">
+                                <input type="text" name="name" class="form-control field-name" placeholder="Name">
+                            </div>
+                            <div class="col-md-6 m-0 p-2">
+                                <input type="email" name="email" class="form-control field-email" placeholder="Email">
+                            </div>
+                            <div class="col-md-6 m-0 p-2">
+                                <input type="text" name="phone" class="form-control field-phone" placeholder="Phone">
+                            </div>
+                            <div class="col-md-6 m-0 p-2">
+                                <i class="icon-arrow-down mr-3"></i>
+                                <select name="info" class="form-control field-info">
+                                    <option value="" selected="" disabled="">More Info</option>
+                                    <option>Audit &amp; Assurance</option>
+                                    <option>Financial Advisory</option>
+                                    <option>Analytics and M&amp;A</option>
+                                    <option>Middle Marketing</option>
+                                    <option>Legal Consulting</option>
+                                    <option>Regulatory Risk</option>
+                                    <option>Other</option>
+                                </select>
+                            </div>
+                            <div class="col-md-12 m-0 p-2">
+                                <textarea name="message" class="form-control" placeholder="Message"></textarea>
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button class="btn btn-primary" type="button">Send</button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
-                <div class="col-12 col-md-4">
-                    <div class="contacts">
+                <div class="col-md-6">
+                    <div class="contacts px-4">
                         <h4>Example Inc.</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         <p>Praesent diam lacus, dapibus sed imperdiet consectetur.</p>
@@ -150,13 +127,13 @@
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
                                     <i class="fas fa-phone-alt mr-2"></i>
-                                    +1 (305) 1234-5678
+                                    +880 1322846601
                                 </a>
                             </li>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
                                     <i class="fas fa-envelope mr-2"></i>
-                                    hello@example.com
+                                    supremeglobalbd@gmail.com
                                 </a>
                             </li>
                             <li class="nav-item">
@@ -173,52 +150,6 @@
                     </div>
                 </div>
             </div>
-
-            <form class="row g-3">
-                <div class="col-md-6">
-                    <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress2" class="form-label">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2"
-                        placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">State</label>
-                    <select id="inputState" class="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="inputZip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
-                </div>
-                <div class="col-12">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </div>
-            </form>
         </div>
     </section>
 @endsection
