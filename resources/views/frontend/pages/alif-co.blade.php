3b35afded90a0e1d461b56b80f7ef92ee45960bc
supremeGlobal <supremeglobalbd@gmail.com>
2025-11-08 12:20:36 +0600

    Alif & Co. page design done

--- resources/views/frontend/pages/alif-co.blade.php
+++ resources/views/frontend/pages/alif-co.blade.php
@@ -7,55 +7,21 @@
     @endphp
     @include('frontend.partials.slider')
 
+
     {{-- about us --}}
     @php
         $topHeading = 'About Us';
         $slogan = 'Your Trusted Partner in Global Trading and Construction Solutions';
-        $order = 29;
     @endphp
     @include('frontend.partials.default')
 
-    {{-- Our Division --}}
+
+    {{-- mission/vision --}}
     @php
-        $heading = 'Our Trading Division';
-        $order = 30;
+        $heading = '';
     @endphp
     @include('frontend.partials.productService')
 
-
-    {{-- Our Construction Division --}}
-    @php
-        $heading = 'Our Construction Division';
-        $order = 31;
-    @endphp
-    @include('frontend.partials.productService')
-
-    {{-- mission --}}
-    @php
-        $heading = 'Our Mission';
-        $slogan = '';
-        $order = 32;
-        $position = 'left';
-    @endphp
-    @include('frontend.partials.singleService')
-
-    {{-- vision --}}
-    @php
-        $heading = 'Our Vision';
-        $slogan = '';
-        $order = 33;
-        $position = 'right';
-    @endphp
-    @include('frontend.partials.singleService')
-
-    {{-- Why Choose us --}}
-    @php
-        $heading = 'Why Choose Alif & Co.?';
-        $slogan = '';
-        $order = 34;
-        $position = 'left';
-    @endphp
-    @include('frontend.partials.singleService')
 
     {{-- client --}}
     @include('frontend.partials.client')
