ccbe932377d2b3c5f0052c7bc2e12bfa7b8ef7f1
supremeGlobal <supremeglobalbd@gmail.com>
2025-11-08 11:57:46 +0600

    Garden Inn page design done

--- resources/views/frontend/pages/garden-inn.blade.php
+++ resources/views/frontend/pages/garden-inn.blade.php
@@ -7,54 +7,21 @@
     @endphp
     @include('frontend.partials.slider')
 
+	
     {{-- about us --}}
     @php
         $topHeading = 'About Us';
         $slogan = 'Where Comfort Meets Adventure';
-        $order = 23;
     @endphp
     @include('frontend.partials.default')
 
-	{{-- mission --}}
-	@php
-		$heading = 'Our Mission';
-		$slogan = '';
-		$order = 24;
-		$position = 'left';
-	@endphp
-	@include('frontend.partials.singleService')
 
-	{{-- vision --}}
+	{{-- mission/vision --}}
 	@php
-		$heading = 'Our Vision';
-		$slogan = '';
-		$order = 25;
-		$position = 'right';
+        $heading = '';
 	@endphp
-	@include('frontend.partials.singleService')
+	@include('frontend.partials.productService')
 
-    {{-- Other information --}}
-    @php
-        $heading = 'What We Offer';
-        $order = 26;
-    @endphp
-    @include('frontend.partials.productService')
-	
-    @php
-        $heading = 'Why Choose Garden Inn Resort and Amusement?';
-        $slogan = '';
-        $order = 27;
-        $position = 'left';
-    @endphp
-    @include('frontend.partials.singleService')
-	
-    @php
-        $heading = 'Book Your Experience';
-        $slogan = '';
-        $order = 28;
-        $position = 'right';
-    @endphp
-    @include('frontend.partials.singleService')
 
     {{-- client --}}
     @include('frontend.partials.client')
